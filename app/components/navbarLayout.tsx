e270c71d95a458ebe2357164ac409a3b1375ff1a
EmmanuelUdeA <emmanuel.bustamante@udea.edu.co>
2024-04-16 21:52:51 -0500

    Se trabajo en el carrito de compras, navbar, y al boton de busqueda

--- app/components/navbarLayout.tsx
+++ app/components/navbarLayout.tsx
@@ -5,12 +5,14 @@ import SubFooter from "../components/subFooter";
 import Menu from "../components/menu";
 import { useState } from "react";
 import CartShop from "../components/cartShop";
+import Ads from "./ads";
 
 const NavbarLayout = ({ children }) => {
     const [viewMenu, setViewMenu] = useState(false);
     const [viewCart, setViewCart] = useState(false);
     return (
         <div className="flex flex-col w-screen h-auto justify-start items-center">
+            <Ads />
             <Navbar viewMenu={viewMenu} setViewMenu={setViewMenu} viewCart={viewCart} setViewCart={setViewCart} />
             {viewMenu && <Menu viewMenu={viewMenu} setViewMenu={setViewMenu} />}
             {viewCart && <CartShop viewCart={viewCart} setViewCart={setViewCart} />}
