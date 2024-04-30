5267ec4fcaf87a74e1d134e5d17a798f0819d027
EmmanuelUdeA <emmanuel.bustamante@udea.edu.co>
2024-04-29 21:55:00 -0500

    Experimentando carga de imagenes en Landing

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -1,11 +1,15 @@
-import Landing1 from "./landing1";
-import Landing2 from "./landing2";
 
 const Landing = () => {
     return (
-        <div className="justify-start overflow-x-hidden flex flex-col items-center w-screen h-auto">
-            <Landing1 />
-            <Landing2 />
+        <div className="justify-start overflow-x-hidden flex flex-row items-center w-screen h-auto flex-wrap">
+            <div className="bg-[url('/LandingImages/landing2blur.jpg')] bg-cover bg-center w-full h-screen blur-load"><img src="/LandingImages/landing2.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing4blur.jpg')] bg-cover bg-center w-full h-screen"><img src="/LandingImages/landing4.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing7blur.jpg')] bg-cover bg-center w-1/2 h-screen"><img src="/LandingImages/landing7.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing8blur.jpg')] bg-cover bg-center w-1/2 h-screen"><img src="/LandingImages/landing8.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing3blur.jpg')] bg-cover bg-center w-full h-screen"><img src="/LandingImages/landing3.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing1blur.jpg')] bg-cover bg-center w-full h-screen"><img src="/LandingImages/landing1.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing6blur.jpg')] bg-cover bg-center w-full h-screen"><img src="/LandingImages/landing6.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
+            <div className="bg-[url('/LandingImages/landing5blur.jpg')] bg-cover bg-center w-full h-screen"><img src="/LandingImages/landing5.jpg" alt="" loading="lazy" className="w-full h-full block" /></div>
         </div>
     )
 }
