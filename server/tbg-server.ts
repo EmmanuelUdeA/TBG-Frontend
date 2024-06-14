f8bf1a3b95afc0e6e66112f6ad21d7988ed15aab
Estiven Muñoz <105648476+Alisser2001@users.noreply.github.com>
2024-06-13 19:06:11 -0500

    Update tbg-server.ts

--- server/tbg-server.ts
+++ server/tbg-server.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const server = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "http://ec2-100-29-11-132.compute-1.amazonaws.com"
 })
 
 export default server;
